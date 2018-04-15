bf529822e08c6ea7977886ea33a9962bc0239bbe
Zack B <jfarris7@students.kennesaw.edu>
2018-04-15 17:35:23 -0400

    added j's sever.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
-var CONTACTS_COLLECTION = "contacts";
+var AuthorizedVeh_Collection = "registeredVehicles";
+var ParkingLot_Collection = "vehiclesInLot";
 
 var app = express();
 app.use(bodyParser.json());
@@ -12,19 +13,99 @@ app.use(bodyParser.json());
 var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://heroku_gs8hmkn3:a76tjk2171270hicfau9b5vv5u@ds143039.mlab.com:43039/heroku_gs8hmkn3", function (err, client) {
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
   }
 
   // Save database object from the callback for reuse.
-  db = client.db();
+  db = database;
   console.log("Database connection ready");
 
   // Initialize the app.
   var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
+  });
+});
+
+// CONTACTS API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/api/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
+
+app.get("/api/registeredVehicles", function(req, res) {
+  db.collection(AuthorizedVeh_Collection).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get authorized vehicle.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.post("/api/registeredVehicles", function(req, res) {
+  var newVehicle = req.body;
+  newVehicle.createDate = new Date();
+
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  }
+
+  db.collection(AuthorizedVeh_Collection).insertOne(newVehicle, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new vehicle.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+/*  "/api/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
+
+app.get("/api/registeredVehicles/:id", function(req, res) {
+  db.collection(AuthorizedVeh_Collection).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contact");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
+
+app.put("/api/registeredVehicles/:id", function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
+
+  db.collection(AuthorizedVeh_Collection).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update vehicles");
+    } else {
+      updateDoc._id = req.params.id;
+      res.status(200).json(updateDoc);
+    }
+  });
+});
+
+app.delete("/api/registeredVehicles/:id", function(req, res) {
+  db.collection(AuthorizedVeh_Collection).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete vehicle");
+    } else {
+      res.status(200).json(req.params.id);
+    }
   });
 });
