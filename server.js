8ccccbb5c074e35232edb36aa622e7b021e62cb6
Zack B <jfarris7@students.kennesaw.edu>
2018-04-19 22:41:54 -0400

    modified vehiclesInLot put method to use variable and check for 'registered' field

--- server.js
+++ server.js
@@ -276,8 +276,8 @@ app.put("/api/registeredVehicles/ID/:id", function(req, res) {
 
 app.put("/api/vehiclesInLot/ID/:id", function(req, res){
 var newReg;
-  if(!req.body.registered){
-    handleError(res, "Invalid request parameters. Please input new status for 'registered'... Value was set to " + req.body.registered, 400);
+  if(!req.registered){
+    handleError(res, "Invalid request parameters. Please input new status for 'registered'... Value was set to " + req.body, 400);
   }else{
     newReg = req.body.registered;
 
